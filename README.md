5a485a3b4fd118feb9deb35729266b24dd55be8c
Robert <robert_zwilling@web.de>
2022-08-25 18:29:59 +0200

    Very Basic Proof of Concept

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # CDSLR2
- Clever DSLR Remote
+
+## Basic Proof of Concept (Windows only)
+Camera must be set to Mass-Storage mode and shoot raw images. The path where Windows mounts the camera must be hard-coded, as well as the path inside the camera's file system where new images can be found. The code then retrieves the newest image from the camera and displays its raw contents (most likely results in false color).
--- software/cdslr2_alpaca/main.py
+++ software/cdslr2_alpaca/main.py
@@ -1,1 +1,38 @@
-print("hello world")
+import os
+import platform
+import rawpy
+import matplotlib.pyplot as plt
+
+
+class MTP_Camera:
+    def __init__(self, mount_point: str, sub_folder: str):
+        self.mountpt = mount_point
+        self.imgpath = os.path.join(mount_point, sub_folder)
+        if not os.path.exists(self.imgpath):
+            FileNotFoundError()
+
+    def get_latest_picture(self):
+        latest_img_path = os.path.join(self.imgpath, os.listdir(self.imgpath)[-1])
+        if not os.path.isfile(latest_img_path):
+            FileNotFoundError()
+        with rawpy.imread(latest_img_path) as raw:
+            #rgb = raw.postprocess()
+            return raw.raw_image.copy()
+
+    def unmount_fs(self):
+        if platform.system() == 'Windows':
+            os.system(
+                'powershell $driveEject = New-Object -comObject Shell.Application;' +
+                '$driveEject.Namespace(17).ParseName("""' + self.mountpt + '""").InvokeVerb("""Eject""")'
+                )
+        else:
+            NotImplementedError('Currently only work on Windows. Sorry.')
+
+
+my_camera = MTP_Camera(mount_point='G:', sub_folder='/DCIM/100MSDCF')
+try:
+    img = my_camera.get_latest_picture()
+    plot = plt.imread(img)
+    plt.show()
+except FileNotFoundError as ex:
+    print(ex)
